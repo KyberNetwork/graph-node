cd450c1c25bf7aedaef7b58dfa0b32d14a3872af
vutran1710 <me@vutr.io>
2023-01-10 16:50:56 +0700

    pass env to specify node functionalities

--- node/src/config.rs
+++ node/src/config.rs
@@ -27,6 +27,13 @@ const ANY_NAME: &str = ".*";
 /// A regular expression that matches nothing
 const NO_NAME: &str = ".^";
 
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub enum NodeFunctionalities {
+    Combined,
+    QueryOnly,
+    Indexer,
+}
+
 pub struct Opt {
     pub postgres_url: Option<String>,
     pub bus_url: Option<String>,
@@ -43,6 +50,7 @@ pub struct Opt {
     pub ethereum_ws: Vec<String>,
     pub ethereum_ipc: Vec<String>,
     pub unsafe_config: bool,
+    pub functionalities: NodeFunctionalities,
 }
 
 impl Default for Opt {
@@ -60,6 +68,7 @@ impl Default for Opt {
             ethereum_ipc: vec![],
             unsafe_config: false,
             bus_url: None,
+            functionalities: NodeFunctionalities::Combined,
         }
     }
 }
@@ -73,6 +82,7 @@ pub struct Config {
     pub stores: BTreeMap<String, Shard>,
     pub chains: ChainSection,
     pub deployment: Deployment,
+    pub functionalities: NodeFunctionalities,
 }
 
 fn validate_name(s: &str) -> Result<()> {
@@ -176,6 +186,7 @@ impl Config {
     }
 
     fn from_opt(opt: &Opt) -> Result<Config> {
+        let functionalities = opt.functionalities.clone();
         let deployment = Deployment::from_opt(opt);
         let mut stores = BTreeMap::new();
         let chains = ChainSection::from_opt(opt)?;
@@ -188,6 +199,7 @@ impl Config {
             stores,
             chains,
             deployment,
+            functionalities,
         })
     }
 
@@ -208,13 +220,17 @@ impl Config {
     }
 
     pub fn query_only(&self, node: &NodeId) -> bool {
-        self.general
-            .as_ref()
-            .map(|g| match g.query.find(node.as_str()) {
-                None => false,
-                Some(m) => m.as_str() == node.as_str(),
-            })
-            .unwrap_or(false)
+        match self.functionalities {
+            NodeFunctionalities::QueryOnly => true,
+            _ => false,
+        }
+        // self.general
+        //     .as_ref()
+        //     .map(|g| match g.query.find(node.as_str()) {
+        //         None => false,
+        //         Some(m) => m.as_str() == node.as_str(),
+        //     })
+        //     .unwrap_or(false)
     }
 }
 
